45ce87349c7d5b0e3d27eef8e56e261e918ef2dd
Franek <frrenk.p@gmail.com>
2019-06-07 10:22:19 +0200

    Added deleting workouts. Closes#4

--- src/pl/piaseckif/controller/Controller.java
+++ src/pl/piaseckif/controller/Controller.java
@@ -13,10 +13,7 @@ import pl.piaseckif.model.ExerciseBuilder;
 import pl.piaseckif.model.ExerciseName;
 import pl.piaseckif.model.Workout;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -39,64 +36,52 @@ public class Controller implements Initializable{
     @FXML private TableColumn<Exercise, Integer> weightColumn;
     @FXML private TableColumn<Exercise, String> durationColumn;
 
-    private List<Exercise> exercises1 = new ArrayList<>();
-    private List<Exercise> exercises2 = new ArrayList<>();
-
-    private Workout workout1 = new Workout();
-    private Workout workout2 = new Workout();
 
     private List<Workout> workouts = new ArrayList<>();
+
+    private Workout currentWorkout;
 
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        System.out.println(123);
+        exerciseColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        setsColumn.setCellValueFactory(new PropertyValueFactory<>("sets"));
+        repsColumn.setCellValueFactory(new PropertyValueFactory<>("reps"));
+        weightColumn.setCellValueFactory(new PropertyValueFactory<>("weight"));
+        durationColumn.setCellValueFactory(new PropertyValueFactory<>("duration"));
+
         workoutDatePicker.setValue(LocalDate.now());
+        loadWorkouts();
+        checkWorkouts();
 
-        exercises1.add(new ExerciseBuilder().setName(ExerciseName.DEADLIFT).setSets(5).setReps(3).setWeight(180).build());
-        exercises1.add(new ExerciseBuilder().setName(ExerciseName.DEADLIFT).setSets(5).setReps(3).setWeight(170).build());
-        exercises1.add(new ExerciseBuilder().setName(ExerciseName.SQUAT).setSets(5).setReps(3).setWeight(150).build());
-        exercises1.add(new ExerciseBuilder().setName(ExerciseName.BENCH_PRESS).setSets(5).setReps(8).setWeight(70).build());
+    }
 
-
-        exercises2.add(new ExerciseBuilder().setName(ExerciseName.JOGGING).setDuration(Duration.of(60, ChronoUnit.MINUTES)).build());
-        exercises2.add(new ExerciseBuilder().setName(ExerciseName.DUMBBELL_BICEP_CURL).setSets(4).setReps(12).setWeight(24).build());
-
-
-        workout1.setExerciseList(exercises1);
-        workout1.setDate(LocalDate.now());
-
-        workout2.setExerciseList(exercises2);
-        workout2.setDate(LocalDate.of(2019, 6, 5));
-
-
-        workouts.add(workout1);
-        workouts.add(workout2);
-
+    private void loadWorkouts() {
         try {
-            FileOutputStream saveWorkout = new FileOutputStream("C:\\Users\\fkpi\\IdeaProjects\\workout-tracker-fx\\resources\\workouts.dat");
-            ObjectOutputStream objOut = new ObjectOutputStream(saveWorkout);
-            objOut.writeObject(workouts);
-            objOut.close();
+            FileInputStream loadWorkout = new FileInputStream("C:\\Users\\fkpi\\IdeaProjects\\workout-tracker-fx\\resources\\workouts.dat");
+            ObjectInputStream inputStream = new ObjectInputStream(loadWorkout);
+            workouts = (List<Workout>)inputStream.readObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveWorkouts() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\fkpi\\IdeaProjects\\workout-tracker-fx\\resources\\workouts.dat");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(workouts);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        exerciseColumn.setCellValueFactory(new PropertyValueFactory<Exercise, ExerciseName>("name"));
-        setsColumn.setCellValueFactory(new PropertyValueFactory<Exercise, Integer>("sets"));
-        repsColumn.setCellValueFactory(new PropertyValueFactory<Exercise, Integer>("reps"));
-        weightColumn.setCellValueFactory(new PropertyValueFactory<Exercise, Integer>("weight"));
-        durationColumn.setCellValueFactory(new PropertyValueFactory<Exercise, String>("duration"));
-
-        //editable rows?
-
-        checkWorkouts();
-
     }
-
 
 
     public void updateTable(ActionEvent actionEvent) {
@@ -109,6 +94,7 @@ public class Controller implements Initializable{
         for (Workout workout:workouts) {
             if (workout.getDate().equals(workoutDatePicker.getValue())) {
                 exerciseTable.getItems().addAll(workout.getExerciseList());
+                currentWorkout = workout;
             }
         }
     }
@@ -127,11 +113,19 @@ public class Controller implements Initializable{
     }
 
     public void exerciseEditStart(TableColumn.CellEditEvent<Exercise, ExerciseName> exerciseExerciseNameCellEditEvent) {
+        exerciseExerciseNameCellEditEvent.getNewValue();
     }
 
     public void exerciseEditCommit(TableColumn.CellEditEvent<Exercise, ExerciseName> exerciseExerciseNameCellEditEvent) {
     }
 
     public void exerciseEditCancel(TableColumn.CellEditEvent<Exercise, ExerciseName> exerciseExerciseNameCellEditEvent) {
+    }
+
+    public void deleteWorkout(ActionEvent actionEvent) {
+        workouts.remove(currentWorkout);
+        updateTable(actionEvent);
+        saveWorkouts();
+
     }
 }
